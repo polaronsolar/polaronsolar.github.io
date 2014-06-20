6ff90595153b99ac5ca3e7954f41b5fa759d3770
Eric Lin <tinyjingle@gmail.com>
2014-06-19 22:47:27 -0400

    adding custom css code

--- script/script.js
+++ script/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+    //Page routing
+    if ($(location).attr('href').indexOf('#faqs') != -1) {
+        $('#content-display').load('faqs.html', function() {
+            //do something
+        });
+    } else if (false) {}
+    else {
+        $('#content-display').load('home.html', function() {
+            init();
+            initAnimate();
+        });
+    }
+
     $('.dropdown').click(function() {
         if ($(this).hasClass('open')) {
             $(this).removeClass('open');
@@ -8,12 +21,19 @@ $(document).ready(function() {
         }
     });
 
+    $('#abc').click(function() {
+        console.log('hi');
+    });
+
+    $(':checkbox').change(function() {
+        console.log('hi');
+    });
+
     var init = function() {
         // Using no-js for IE
         $('#welcome-logo').hide();
         $('#welcome-text').hide();
         $('#welcome-btn').hide();
-
     };
 
     var initAnimate = function() {
@@ -22,8 +42,27 @@ $(document).ready(function() {
         $('#welcome-btn').delay(1200).fadeIn(1500);
     }
 
-    init();
-    $( window ).load(function() {
-        initAnimate();
+    $('.navigate-link').click(function() {
+        var pageStr = $(this).attr('id') + '.html';
+        $('#content-display').load(pageStr, function() {
+            //do something
+        });
     });
+
+    // var loadHomePage = $.ajax({
+    //     url: 'home.html',
+    //     success: function(data) {
+    //         $('#content-display').html(data);
+    //         init();
+    //         initAnimate();
+    //     }
+    // });
+
+    // $('#content-display').load('home.html', function() {
+    //     init();
+    //     initAnimate();
+    // });
+    // // $( window ).load(function() {
+
+    // // });
 });
